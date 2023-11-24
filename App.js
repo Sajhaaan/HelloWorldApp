99aa7ad085046feeecc0d1bdf2649b2f280b788b
Sajhaaan <sajhaaaan@gmail.com>
2023-11-24 15:44:58 +0530

    added some lines in

--- App.js
+++ App.js
@@ -1,23 +1,45 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { Component } from 'react';
+import { SafeAreaView, StyleSheet, View, StatusBar } from 'react-native';
+import { WebView } from 'react-native-webview';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Hello, World!</Text>
-    </View>
-  );
+class App extends Component {
+  state = {
+    contentScript: null // You can initialize your content script here
+  };
+
+  receiveMessage = (event) => {
+    // Handle messages received from WebView here
+    console.log('Received message:', event.nativeEvent.data);
+  };
+
+  render() {
+    const websiteURL = 'https://monkthreads.shop';
+
+    return (
+      <SafeAreaView style={styles.container}>
+        <StatusBar barStyle="dark-content" />
+        <WebView
+          source={{ uri: websiteURL }}
+          style={styles.webview}
+          injectedJavaScript={this.state.contentScript}
+          domStorageEnabled={true}
+          startInLoadingState={true}
+          onMessage={this.receiveMessage}
+          ref={(ref) => (this.webview = ref)}
+        />
+      </SafeAreaView>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
     backgroundColor: '#fff',
   },
-  text: {
-    fontSize: 24,
-    fontWeight: 'bold',
+  webview: {
+    flex: 1,
   },
 });
+
+export default App;
